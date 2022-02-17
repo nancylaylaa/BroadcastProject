f6e59208cca3270980f75eb62ce4efc329dffd4b
Nancy <nancy.laylana.putri@mail.ugm.ac.id>
2022-02-17 16:12:53 +0700

    Initial commit

---
+++ app/src/main/java/com/papb/broadcastproject/CustomReceiver.java
@@ -0,0 +1,30 @@
+package com.papb.broadcastproject;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Switch;
+import android.widget.Toast;
+
+public class CustomReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+        String msg = "This is default message";
+        switch (intent.getAction()){
+            case Intent.ACTION_POWER_CONNECTED:
+                msg = "the power is connected";
+                break;
+            case Intent.ACTION_POWER_DISCONNECTED:
+                msg = "the power is disconnected";
+                break;
+            case "ACTION_CUSTOM_BROADCAST":
+                msg = "this is custom broadcast";
+            default:
+                break;
+        }
+        Toast.makeText(context, msg, Toast.LENGTH_SHORT).show();
+    }
+}
